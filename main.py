87a2b570d90a4668d8204f944a6336ece8686061
khubaibwaqarkhan <khubaib.waqar.cowlar@gmail.com>
2025-10-05 12:32:36 -0400

    testing login page

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from src.calendar.routes import router as calendar_router
+
+app = FastAPI()
+templates = Jinja2Templates(directory="pages")
 
 app = FastAPI(title="Google Calendar Integration API")
 
 # Register routes
 app.include_router(calendar_router, prefix="", tags=["Calendar"])
 
-@app.get("/")
-def root():
-    return {"message": "Google Calendar API Backend is running!"}
+@app.get("/", response_class=HTMLResponse)
+async def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
 
 if __name__ == "__main__":
     import uvicorn
