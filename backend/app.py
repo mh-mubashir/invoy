ae6e47b547e4e2051608148f56379ba2c3fb341b
khubaibwaqarkhan <khubaib.waqar.cowlar@gmail.com>
2025-10-05 16:36:45 -0400

    fix app load

--- backend/app.py
+++ backend/app.py
@@ -28,10 +28,9 @@ app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_methods=['*'], all
 # Register routes
 app.include_router(calendar_router, prefix="", tags=["Calendar"])
 
-# app.mount("/assets", StaticFiles(directory="assets"), name="assets")
-# @app.get("/", response_class=HTMLResponse)
-# async def get_login_page(request: Request):
-#     return templates.TemplateResponse("login.html", {"request": request})
+@app.get("/login", response_class=HTMLResponse)
+async def get_login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
 
 # No-cache for HTML to always serve fresh UI
 @app.middleware("http")
